ba59739f40313e3df777c67e5ae0162e6f0501e2
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-16 02:48:57 +0200

    init

---
+++ src/isExists/test.ts
@@ -0,0 +1,16 @@
+/* eslint-env mocha */
+
+import assert from 'assert'
+import isExists from './index'
+
+describe('isValid', () => {
+  it('returns true if the given date is valid', () => {
+    const result = isExists(2018, 0, 31)
+    assert(result === true)
+  })
+
+  it('returns false if the given date is invalid', () => {
+    const result = isExists(2018, 1 /* Feb */, 31)
+    assert(result === false)
+  })
+})
